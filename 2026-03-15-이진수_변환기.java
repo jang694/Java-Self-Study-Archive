83c2b3a95cc0196562b94f1a609cc6a4a8bc55e4
jang694 <105403223+jang694@users.noreply.github.com>
2026-03-15 20:11:00 +0900

    Update 2026-03-15-이진수_변환기.java

--- 2026-03-15-이진수_변환기.java
+++ 2026-03-15-이진수_변환기.java
@@ -11,18 +11,24 @@ public class Test {
        int b = 0;
        int c = 0;
        int count = 0;
+       String binary = "";
        System.out.print("이진수로 변환활 숫자를 입력하시오>>");
        a = sc.nextInt();
+       System.out.print("바꾸기 전 이진수:");
        while (a  > 0) {//a/2>0이 안되는 이유는 a가 1일때 1/2 즉 0.5니깐 int에 적용하면 0이므로 0>0은 (false)여서 반복문 종료됨
         b = a / 2;
         c= a % 2;
         a = a/2;//처음에는 b=a/2만 적었지만 그러면 안되는 이유는 b는 작아지는데 a는 그대로 유지하기에 a도 같이 작아져야 하기 때문
+
         if(c == 1){
-            ++count;//1이 몇개인지 세는 코드
+            ++count;
         }
-        System.out.print(c);/1011 이면 1101로 출력됨
+        binary = c + binary;
+        System.out.print(c);
        }
        System.out.println();
+       System.out.println("최종 이진수:"+binary);// "0"+""-> "0" "1"+"0"->"10" 이런식으로 더해 져서 0101이 1010으로 써질 수 있음
+       
        System.out.println(count+"개");
     }   
 }
